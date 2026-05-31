90a2a5182bf6dc846649b3b49caa804c5b564e33
qwepyhbvc <qwe248254@gmail.com>
2026-05-31 16:57:23 +0300

    Add comments to code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
 
+// Main function: entry point of the program
 int main()
 {
+    // Variable to store user name
     std::string name;
+    
+    // Prompt user for input
     std::cout << "Enter your name: ";
     std::cin >> name;
+    
+    // Output greeting message
     std::cout << "Hello world from " << name << "!" << std::endl;
-    return 0;
+    
+    return 0; // Successful execution
 }
